afe7708d8b363f2caf80aae4b766d4db3c004a38
Łukasz Dudziak <lukasz@a2labs.ai>
2025-04-02 14:22:08 +0100

    Trying out more stuff

--- scripts/ci/static-analysis.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -xe
-
-source .venv/bin/activate
-
-mypy mako_engine --junit-xml=junit/mypy-results.xml
--- scripts/ci/tests.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -xe
-
-source .venv/bin/activate
-
-pytest tests/ --junitxml=junit/test-results.xml
